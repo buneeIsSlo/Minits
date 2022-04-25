32f7541c7c5d9cf3cdd3838b2d347b5b25b6f03e
bunee <buneeisslo@gmail.com>
2022-04-25 22:28:12 +0530

    Refactor: add all code from nav.js to timer.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,8 @@ import "../css/sanitize.css";
 import "../css/custom.css";
 import "../css/main.css";
 
-import Nav from "./nav.js";
 import Timer from "./timer.js"
 
 
 console.log("bunee!");
-const nav = new Nav();
 const timer = new Timer();
--- src/js/timer.js
+++ src/js/timer.js
@@ -8,60 +8,112 @@ export default class Timer {
 
     vars() {
         this.selectors = {
+            navBtns: ".minits--nav-btn",
+            pomodoroBtn: ".minits__pomodoro",
             minitsTime: ".minits__time",
             timerControl: ".minits__pause-play",
             resetControl: ".minits__reset",
             playSVG: "play-icon",
-            pauseSVG: "pause-icon"
+            pauseSVG: "pause-icon",
+            utilActive: "active",
+            utilHide: "hide",
+            utilTimerRunning: "running",
         }
 
+        this.navBtns = document.querySelectorAll(`${this.selectors.navBtns}`);
+        this.pomodoro = document.querySelector(`${this.selectors.pomodoroBtn}`);
         this.minitsTime = document.querySelector(`${this.selectors.minitsTime}`);
         this.timerControl = document.querySelector(`${this.selectors.timerControl}`);
         this.resetControl = document.querySelector(`${this.selectors.resetControl}`);
         this.playSVG = document.getElementById(`${this.selectors.playSVG}`);
         this.pauseSVG = document.getElementById(`${this.selectors.pauseSVG}`);
 
-        this.startTime = this.minitsTime.dataset.secondsLeft;
-
+        if (!this.navBtns || !this.pomodoro || !this.minitsTime || !this.timerControl || !this.resetControl) return false;
         return true;
     }
 
     setUpEvents() {
+        this.init();
+
+        this.handleNav();
+
         this.timerControl.addEventListener("click", () => {
-            this.toggle();
-
-            this.minitsTime.classList.toggle("running");
-
-            this.beginCountdown(+this.minitsTime.dataset.secondsLeft);
+            this.handleTimerControl();
         });
 
         this.resetControl.addEventListener("click", () => {
-            const startTime = this.minitsTime.dataset.secondsStart;
-
-            this.playSVG.classList.remove("hide");
-            this.pauseSVG.classList.add("hide");
-
-            this.minitsTime.classList.remove("running");
-
-            this.setTime(startTime);
-            this.minitsTime.dataset.secondsLeft = startTime;
+            this.handleResetControl();
         })
+    }
+
+    init() {
+        this.minitsTime.dataset.secondsStart = this.toSeconds(this.pomodoro.dataset.time);
+        this.minitsTime.dataset.secondsLeft = this.toSeconds(this.pomodoro.dataset.time);
+        this.setTime(this.minitsTime.dataset.secondsLeft);
+    }
+
+    handleNav() {
+        this.navBtns.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                this.addActiveClass(btn);
+                this.minitsTime.classList.remove(this.selectors.utilTimerRunning);
+
+                let dataTime = this.toSeconds(btn.dataset.time);
+
+                this.minitsTime.dataset.secondsStart = dataTime;
+                this.minitsTime.dataset.secondsLeft = dataTime;
+                this.setTime(dataTime);
+            })
+        });
+    }
+
+    handleTimerControl() {
+        this.playSVG.classList.toggle(this.selectors.utilHide);
+        this.pauseSVG.classList.toggle(this.selectors.utilHide);
+
+        this.minitsTime.classList.toggle(this.selectors.utilTimerRunning);
+
+        this.beginCountdown(this.minitsTime.dataset.secondsLeft);
+    }
+
+    handleResetControl() {
+        const startTime = this.minitsTime.dataset.secondsStart;
+
+        this.switchSVG();
+
+        this.minitsTime.classList.remove(this.selectors.utilTimerRunning);
+
+        this.setTime(startTime);
+        this.minitsTime.dataset.secondsLeft = startTime;
+    }
+
+    addActiveClass(element) {
+        this.removeExistingActiveClass();
+        element.classList.add(`${this.selectors.utilActive}`);
+    }
+
+    removeExistingActiveClass() {
+        this.navBtns.forEach((btn) => {
+            btn.classList.remove("active");
+        });
+    }
+
+    toSeconds(t) {
+        return t * 60;
     }
 
     beginCountdown(time) {
         const now = Date.now();
         const endTime = now + (time * 1000);
-        let secondsLeft;
 
         const countdown = setInterval(() => {
-            if (this.minitsTime.classList.contains("running")) {
-                secondsLeft = Math.round((endTime - Date.now()) / 1000);
+            if (this.minitsTime.classList.contains(this.selectors.utilTimerRunning)) {
+                let secondsLeft = Math.round((endTime - Date.now()) / 1000);
                 this.setTime(secondsLeft);
                 this.minitsTime.dataset.secondsLeft = secondsLeft;
             }
             else {
-                this.playSVG.classList.remove("hide");
-                this.pauseSVG.classList.add("hide");
+                this.switchSVG();
                 clearInterval(countdown);
             }
         }, 1000);
@@ -82,8 +134,9 @@ export default class Timer {
         this.minitsTime.innerHTML = `${minutes}:${seconds}`;
     }
 
-    toggle() {
-        this.playSVG.classList.toggle("hide");
-        this.pauseSVG.classList.toggle("hide");
+    switchSVG() {
+        this.playSVG.classList.remove(this.selectors.utilHide);
+        this.pauseSVG.classList.add(this.selectors.utilHide);
     }
+
 }
