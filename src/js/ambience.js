af071f31f91fd9b4160d474680109ce4fbba2504
bunee <buneeisslo@gmail.com>
2022-11-05 17:35:47 +0530

    Test: code radio by freeCodeCamp
    Pulling data from coderadio API to play music

--- src/js/ambience.js
+++ src/js/ambience.js
@@ -10,11 +10,13 @@ export default class Ambience {
             ambienceBtn: ".minits__ambience",
             ambienceOnIcon: "ambienceOnIcon",
             ambienceOffIcon: "ambienceOffIcon",
+            ambienceAudo: "ambience"
         };
 
         this.ambienceBtn = document.querySelector(this.selectors.ambienceBtn);
         this.ambienceOnIcon = document.getElementById(this.selectors.ambienceOnIcon);
         this.ambienceOffIcon = document.getElementById(this.selectors.ambienceOffIcon);
+        this.ambienceAudio = document.getElementById(this.selectors.ambienceAudo);
 
         return true;
     }
@@ -24,9 +26,31 @@ export default class Ambience {
     }
 
     handleAmbienceBtn() {
-        this.ambienceBtn.addEventListener("click", () => {
+        this.ambienceBtn.addEventListener("click", async () => {
             this.ambienceOnIcon.classList.toggle("hide");
             this.ambienceOffIcon.classList.toggle("hide");
+
+            this.playCodeRadio();
         });
+    }
+
+    async playCodeRadio() {
+
+        if (this.ambienceOnIcon.classList.contains("hide")) {
+            this.ambienceAudio.src = "";
+            this.ambienceAudio.pause();
+            return;
+        }
+
+        const API_URL = "https://coderadio-admin.freecodecamp.org/api/live/nowplaying/coderadio";
+
+        const response = await fetch(API_URL, { cache: "no-cache" });
+        const json = await response.json();
+
+        console.log(json);
+        const LISTEN_URL = json.station.listen_url;
+
+        this.ambienceAudio.src = LISTEN_URL;
+        this.ambienceAudio.play();
     }
 }
